e21fddddd5bba631a2e93237df75960d6062e06a
vitaliynezhinskiy <SeekerOfConfessor@gmail.com>
2020-05-02 16:00:36 +0300

    add observer + observable

--- src/main/java/ClientEntity.java
+++ src/main/java/ClientEntity.java
@@ -4,28 +4,40 @@ import lombok.SneakyThrows;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 @AllArgsConstructor
-public class ClientEntity implements Runnable {
+public class ClientEntity implements Runnable, Observer {
     private final Socket socket;
+    private final MyServer myServer;
     private final Client user = new Client();
 
     @SneakyThrows
     @Override
     public void run() {
         BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String input;
-        while ((input = socketReader.readLine()) != null) {
+        String messageFromClient;
+        while ((messageFromClient = socketReader.readLine()) != null) {
             //!@#REGISTRATION%^*!vitalik::qwerty
-            if (input.startsWith("!@#REGISTRATION%^*!")) {
-                String[] logAndPass = input.substring(19).split("::");
+            if (messageFromClient.startsWith("!@#REGISTRATION%^*!")) {
+                String[] logAndPass = messageFromClient.substring(19).split("::");
                 System.out.println(logAndPass[0] + " " + logAndPass[1]);
                 user.setLogin(logAndPass[0]);
                 user.setPassword(logAndPass[1]);
+                myServer.registerObserver(this);
             } else {
-                System.out.println(input);
+                System.out.println(messageFromClient);
+                myServer.notifyObservers(messageFromClient);
             }
         }
+    }
+
+    @SneakyThrows
+    @Override
+    public void notifyObserver(String message) {
+        PrintWriter socketWriter = new PrintWriter(socket.getOutputStream());
+        socketWriter.println(message);
+        socketWriter.flush();
     }
 }
--- src/main/java/MyServer.java
+++ src/main/java/MyServer.java
@@ -1,23 +1,40 @@
 import lombok.SneakyThrows;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MyServer {
+public class MyServer implements Observable {
     public static final int PORT = 8080;
+    public static volatile List<Observer> clients = new ArrayList<>();
 
     @SneakyThrows
     public void start() {
         ServerSocket serverSocket = new ServerSocket(PORT);
-        Socket socket = null;
 
         while (true) {
-            socket = serverSocket.accept();
+            Socket socket = serverSocket.accept();
             if (socket != null) {
-                new Thread(new ClientEntity(socket)).start();
+                new Thread(new ClientEntity(socket, this)).start();
             }
+        }
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        clients.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        clients.remove(o);
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (Observer observer : clients) {
+            observer.notifyObserver(message);
         }
     }
 }
